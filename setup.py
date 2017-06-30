a82be021435ebee766c22b267e7b4992f43e364f
nasseralkmim <nasser.alkmim@gmail.com>
2017-06-30 18:12:47 -0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='meshplotlib',
+      version='0.1',
+      author='Nasser Alkmim',
+      license='GPL3',
+      py_modules=['meshplotlib'],
+      install_requires=['matplotlib', 'numpy'])
